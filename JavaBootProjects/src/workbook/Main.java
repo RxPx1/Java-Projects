3351c1a920a00fec3f258df0c6c92a11730c50a3
Rob <110516770+RxPx1@users.noreply.github.com>
2022-09-08 23:33:22 -0400

    Changes to Projects and Adding new Ones

--- JavaBootProjects/src/workbook/Main.java
+++ JavaBootProjects/src/workbook/Main.java
@@ -1,44 +1,34 @@
 package workbook;
 
-import java.util.Arrays;
-
-
 public class Main {
 
 	public static void main(String[] args) {
-	
-		Person passenger1 = new Person("Rayan Slim", "Canadian", "01/01/1111", 5);
-		passenger1.chooseSeat();
-		
-		Person passenger2 = new Person(passenger1);
-		passenger2.chooseSeat();
-		
-		passenger2.setName("Jad Slim");
-		
-		
-		System.out.println("Name: " + passenger1.getName() + "\n" + "Nationality: " + passenger1.getNationality() + "\n" + "Date of birth: " + passenger1.getDateOfBirth() +"\n" + "Seat number: " + passenger1.getSeatNumber());
-		
-		System.out.println();
-		
-		if (passenger1.applyPassport() == true) {
-			System.out.println("Congratulations " + passenger1.getName() + ". Your passport was approved!");
-		} else {
-			System.out.println("We're sorry " + passenger1.getName() + ". We cannot process your application.");
-		}
-		System.out.println();
-		System.out.println("Name: " + passenger2.getName() + "\n" + "Nationality: " + passenger2.getNationality() + "\n" + "Date of birth: " + passenger2.getDateOfBirth() +"\n" + "Seat number: " + passenger2.getSeatNumber());
-		System.out.println();
-		
-		if (passenger2.applyPassport() == true) {
-			System.out.println("Congratulations " + passenger2.getName() + ". Your passport was approved!");
-		} else {
-			System.out.println("We're sorry " + passenger2.getName() + ". We cannot process your application.");
-		}
-		
-		
-		
-	
-
-	}
-
-}
+        Person[] people = new Person[] {
+                new Person("Cleopatra", "Egypt", "69 BC", 1),
+                new Person("Alexander the Great", "Macedon", "356 BC", 1),
+                new Person("Julius Caesar", "Rome", "100 BC", 1),
+                new Person("Hannibal", "Carthage", "247 BC", 1),
+                new Person("Confucius", "China", "551 BC", 1),
+                new Person("Pericles", "Greece", "429 BC", 2),
+                new Person("Spartacus", "Thrace", "111 BC", 2),
+                new Person("Marcus Aurelius", "Rome", "121 AD", 2),
+                new Person("Leonidas", "Greece", "540 BC", 2),
+                new Person("Sun Tzu", "China", "544 BC", 2),
+                new Person("Hammurabi", "Babylon", "1750 BC", 2)
+            };
+     
+            Airline airline = new Airline();
+     
+            for (int i = 0; i < people.length; i++) {
+                if (people[i].applyPassport() == true) {
+                    people[i].setPassport();
+                    airline.createReservation(people[i]);
+                }
+            }
+     
+            System.out.println("********************** RESERVATIONS COMPLETE! **********************\n");
+            System.out.println(airline);
+     
+        }
+      
+    }
